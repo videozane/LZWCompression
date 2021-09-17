a5a2c2ea33292befecf16c8b52164715e028ab9b
chris-connoll-y <89544126+chris-connoll-y@users.noreply.github.com>
2021-09-16 20:26:34 -0700

    finished final version

--- LZWCompression/src/Decoder.java
+++ LZWCompression/src/Decoder.java
@@ -1,4 +1,123 @@
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 public class Decoder {
-
+	private int sizeOfByte;
+	private HashMap<Integer, String> dictionary;
+	private byte[] fileContents;
+	private int sizeOfDictionary;
+	
+	
+	public Decoder(String fileName) {
+		
+		sizeOfDictionary = 0;
+		//code below puts in normal chars into the table
+		dictionary = new HashMap<Integer, String> ();
+		for(int index = 0; index < 256; index++) {
+			char newChar = (char)index; //changes the index of ASCII to a character
+			String toBeAdded = "" + newChar; //changes the character to a string
+			dictionary.put(index, toBeAdded); //puts string into HashMap with corresponding spot in table
+			sizeOfDictionary++;
+		}
+		
+		
+		
+		/**
+		 * puts all of the bytes of the file into an array of bytes
+		 */
+		Path path = Paths.get(fileName);
+		System.out.println(path.toAbsolutePath());
+		try {
+			fileContents =  Files.readAllBytes(path);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		/**
+		 * turns array of bytes into one long string of 0s and 1s
+		 */
+		StringBuilder strBuilder = new StringBuilder ();
+		for (int i = 0; i < fileContents.length; i ++) {
+			byte b1 = fileContents [i];
+			strBuilder.append(String.format("%8s", Integer.toBinaryString(b1 & 0xFF)).replace(' ', '0'));
+		}
+		
+		
+		
+		System.out.println(decode (strBuilder.toString()));
+		
+	}
+	
+	
+	/**
+	 * 
+	 * 
+	 * @param toBeDecoded is the String that was created by the binary file
+	 * @return the decoded message
+	 */
+	public String decode (String toBeDecoded) {
+		
+		
+		StringBuilder toRet = new StringBuilder ();
+		
+		/**
+		 * parses String into proper size of each byte
+		 */
+		ArrayList<String> parsedStr = new ArrayList<String>();
+		for (int i = 0; i < toBeDecoded.length()-sizeOfByte; i+=sizeOfByte) {
+			parsedStr.add(toBeDecoded.substring(i,sizeOfByte));
+		}
+		
+		/**
+		 * converts parsed String into ints
+		 */
+		ArrayList<Integer> parsedInts = binStringsToIntegers (parsedStr);
+		String current = "";
+		String next = "";
+		for (int i = 0; i < parsedInts.size()-1; i++) {
+			current = dictionary.get(parsedInts.get(i));
+			next= dictionary.get(parsedInts.get(i+1));
+			String firstOfNext = next.charAt(0) + "";
+			if (!dictionary.containsValue(current + firstOfNext)) {
+				dictionary.put(sizeOfDictionary, current + firstOfNext);
+				sizeOfDictionary++;
+			}
+			toRet.append(current);
+		}
+		toRet.append(next);
+		
+		return toRet.toString();
+		
+	}
+	
+	public static ArrayList<Integer> binStringsToIntegers (ArrayList<String> input){
+		ArrayList<Integer> toRet = new ArrayList<Integer>();
+		
+		
+		String binaryString = "";
+        double convertedDouble = 0;
+        for (int j = 0; j < input.size(); j++) {
+        	binaryString = input.get(j);
+        	convertedDouble = 0;
+	        for (int i = 0; i < binaryString.length(); i++) {
+	
+	            if (binaryString.charAt(i) == '1') {
+	                int len = binaryString.length() - 1 - i;
+	                convertedDouble += Math.pow(2, len);
+	            }
+	        }
+	
+	        int convertedInt = (int) convertedDouble;
+	        toRet.add(convertedInt);
+        }
+		return toRet;
+	}
+	
+	public static void main (String [] args) {
+		Decoder tester = new Decoder ("output.bin");
+	}
 }
